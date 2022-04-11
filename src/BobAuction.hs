b09785d2a2cf2637d2d169b44e7e122ed2571c77
Bereket Godebo <bereket.godebo@outlook.com>
2022-04-11 07:59:12 +0300

    ADTs to represent auctioning

--- src/BobAuction.hs
+++ src/BobAuction.hs
@@ -1,3 +1,9 @@
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DeriveAnyClass             #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TemplateHaskell            #-}
+
 module BobAuction 
      (
 
@@ -24,4 +30,59 @@ import           PlutusTx.Prelude     hiding (unless)
 import qualified Prelude              as P
 import           Schema               (ToSchema)
 import           Text.Printf          (printf)
+
+-- an NFT must contain a minimum amount ada
+minLovelace :: Integer
+minLovelace = 2000000
+
+
+-- The auction must be parametrized by the owner of the token,
+-- the token itself, mininum bid amount and the deadline.
+-- A token is represented by CurrencySymbol and TokenName
+
+data Auction = Auction
+    { aSeller   :: !PaymentPubKeyHash
+    , aDeadline :: !POSIXTime
+    , aMinBid   :: !Integer
+    , aCurrency :: !CurrencySymbol
+    , aToken    :: !TokenName
+    } deriving (P.Show, Generic, ToJSON, FromJSON, ToSchema)
+
+-- defining equality for Auction type
+instance Eq Auction where 
+    {-# INLINABLE (==) #-}
+    a == b = (aSeller   a == aSeller   b) &&
+             (aDeadline a == aDeadline b) &&
+             (aMinBid   a == aMinBid   b) &&
+             (aCurrency a == aCurrency b) &&
+             (aToken    a == aToken    b) 
+
+-- Generate FromData and ToData instance for Auction type
+PlutusTx.unstableMakeIsData ''Auction
+-- Class for types which can be lifted into Plutus IR
+PlutusTx.makeLift ''Auction 
+
+data Bid = Bid 
+    { aBidder :: !PaymentPubKeyHash
+    , aBid    :: !Integer 
+    } deriving P.Show 
+
+instance Eq Bid where 
+    {-# INLINABLE (==) #-}
+    b == c = (aBidder b == aBidder c) &&
+             (aBid    b == aBid    c)
+
+PlutusTx.unstableMakeIsData ''Bid
+PlutusTx.makeLift ''Bid
+
+-- bidders can either bid or close the auction
+-- it does not matter if anyone invokes the close action
+-- since the contract only closes an auction after the deadline 
+data AuctionAction = MkBid Bid | Close 
+
+PlutusTx.unstableMakeIsData ''AuctionAction
+PlutusTx.makeLift ''AuctionAction
+
+
+
 
